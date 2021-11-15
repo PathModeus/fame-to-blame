7fee250c1af79f367ee6e0d4fd74711f876f0956
Mathilde Jacquotot <mathilde.jacquotot@student-cs.fr>
2021-11-15 16:14:54 +0100

    adding csv to list and insult detection and update swear words database

--- src/insult_detection/insult_detection.py
+++ src/insult_detection/insult_detection.py
@@ -2,28 +2,45 @@ from textblob import TextBlob
 from textblob import Word
 
 
-from Data.swear_words_database import convert_database
+from csv_to_list import convert_database
 
 path='Data/swear_words_database.csv'
 swear_words_data=convert_database(path)
 def detect_insult_tweet(tweet):
+    """Return a list of the insults contained in a tweet
+    Parameters
+    ----------
+    tweet : a dataframe panda with a key 'Texte'
+
+    Returns
+    --------
+    insults : a list of strings
+    """
     insults=[]
     sentence=TextBlob(tweet['Texte'])
+    str_tweet=""
     for word in sentence.words:
         word=word.singularize
         word=Word(word)
         word=word.lemmatize
-        if word in swear_words_data:
-            insults.append(word)
+        str_tweet+=word
+        for insult in swear_words_data:
+            if insult in str_tweet:
+                insults.append(insult)
     return insults
 
 def detect_insults_tweets(data):
+    """Return a  dictionnary of key celebrity_ID and for each key containing a list of lists
+        Each list of list contains the insults in one tweet
+    Parameters
+    ----------
+    tweet : a dataframe panda with a key 'Texte'
+
+    Returns
+    --------
+    insults : a dictionnary of lists of lists
+    """
     data_insults={}
     for i in range(len(data)):
-        data_insults[str(i)]=[detect_insult_tweet(tweet) for tweet in data[i][2]]
+        data_insults[str(i+1)]=[detect_insult_tweet(tweet) for tweet in data[i][2]]
     return data_insults
-
-print ('ok')
-
-
-
