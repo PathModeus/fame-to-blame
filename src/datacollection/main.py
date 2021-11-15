30b389e9eff0cf878a5a73cb5da564e704a03491
Debiais Gaspard <gaspard.debiais@student-cs.fr>
2021-11-15 15:07:00 +0100

    Adaptation collection

--- src/datacollection/main.py
+++ src/datacollection/main.py
@@ -5,12 +5,22 @@ import pandas as pd
 
 def collection(celebrities_amount):
     collect=[]
+<<<<<<< HEAD
     for i in range(1,celebrities_amount + 1):
         num_celebrity = str(i)
         twitter_api = twitter_setup('C:/Users/gaspa/OneDrive/Documents/CS/Coding Weeks') #On se connecte à l'API
         keywords = get_candidate_queries(num_celebrity, 'Data/keywords_celebrity_')  #On récupère les mots-clés à rechercher
         name=keywords[0]
         result=get_tweets_from_candidates_search_queries(keywords,twitter_api)+get_replies_to_candidate(num_celebrity,twitter_api)+get_retweets_of_candidate(num_celebrity,twitter_api) #Renvoie une liste contenant tous les statuts récoltés
+=======
+    for i in range(celebrities_amount + 1):
+        num_celebrity = str(i)
+        twitter_api = twitter_setup() #On se connecte à l'API
+        keywords = get_candidate_queries(num_celebrity, 'Data/keywords_celebrity_')  #On récupère les mots-clés à rechercher
+        name=keywords[0]
+        result=get_tweets_from_candidates_search_queries(keywords,twitter_api)+get_replies_to_candidate(num_celebrity,twitter_api)+get_retweets_of_candidate(num_celebrity,twitter_api)
+        result=result+stream(keywords,5) #Renvoie une liste contenant tous les statuts récoltés
+>>>>>>> f72ffbd (Adaptation collection)
         tweets=[]
         for status in result:
             try:
@@ -19,7 +29,12 @@ def collection(celebrities_amount):
                 tweets.append(json.loads(status.decode('utf-8')))
         storeTweet(tweets,name)
         dataframe=pd.DataFrame.from_dict(extractjson('Data/' + name + '.json'))
+<<<<<<< HEAD
         collect.append([int(num_celebrity),name,dataframe])
     return collect
 
 print(collection(3))
+=======
+        collect.append([num_celebrity,name,dataframe])
+    return collect
+>>>>>>> f72ffbd (Adaptation collection)
